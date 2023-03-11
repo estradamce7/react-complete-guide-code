257e2dba6f337755f9229d03a60bfbfe14e20022
estradamce7 <estradamce7@gmail.com>
2023-03-11 00:34:31 -0600

    Notes for 06-stylling
    Notes and progress

--- code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ code/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+// import styled from 'styled-components'; // not needed with moduled css
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import styles from './CourseInput.module.css';
+// import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// // styled components always passes all props
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : 'black'};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/*<div className={`form-control ${!isValid ? 'invalid' : ''}`}>*/} {/* `` - called "backticks" adds the string literate and a special ${} can be added for conditional/jsx */}
+      <div className={ `${styles['form-control']} ${!isValid && styles.invalid}` }> {/* "form-control" in styles.form-control is not a valid css, instead wrap in square brackets */}
+      {/* <FormControl invalid={!isValid}> */}
+        {/*inline style can use condition*/}
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
+      {/* </FormControl>   */}
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
--- code/01-starting-project/src/components/UI/Button/Button.js
+++ code/01-starting-project/src/components/UI/Button/Button.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from 'react'; // not needed because we are not using JSX here
+import styles from './Button.module.css' // this way to import from a css file if you want to use CSS Modules | also, need to rename the css to add ".module" - this transforms the code so CSS modules work
+// import styled from "styled-components";
 
-import './Button.css';
+// import './Button.css';
+
+// // 2 backticks is called attacked template - a method in a styled object. can be used instead of ()
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `; // & is for pseudo element
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}> {/* className={styles.button} adds the class defined in the Button.module.css */}
       {props.children}
     </button>
   );
