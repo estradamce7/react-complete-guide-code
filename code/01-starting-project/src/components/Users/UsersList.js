f469b520f9f3518d7491c0624241a34c8af9b7ee
estradamce7 <estradamce7@gmail.com>
2023-03-11 22:09:10 -0600

    Practice Project 1 - missed files

---
+++ code/01-starting-project/src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import classes from "./UsersList.module.css";
+import Card from "../UI/Card";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          // key warning/error - key should be added here because this renders the list
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
