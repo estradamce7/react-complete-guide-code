1040bd5fb8acf6fe78fb96259814901dea3cd943
estradamce7 <estradamce7@gmail.com>
2023-03-24 22:41:24 -0500

    23-deploying-react-apps-updated-with_mynotes
    Seciont 22 with notes

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,13 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home";
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+
+// lazy is executed and takes the fn with dynamic import as an arg
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +20,29 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          // { index: true, element: <BlogPage />, loader: postsLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ), // Suspense - component by React that can be used by other components to wait for content to be loaded before rendering
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          }, // lazy load the postsLoader. import fn always returns a promise. in import we pass in the path, since it is async, we will add then. from there, we load the module => module.loader() (loading the module)
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
